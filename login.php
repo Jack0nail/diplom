4b626d6dd2d9c666870032825dc038bb42cb2a22
Jack0nail <kirsid2000@mail.ru>
2020-05-18 15:51:46 +0300

    first commit

---
+++ login.php
@@ -0,0 +1,108 @@
+<?php
+// Страница авторизации
+
+// Функция для генерации случайной строки
+function generateCode($length=6) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+    $code = "";
+    $clen = strlen($chars) - 1;
+    while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0,$clen)];
+    }
+    return $code;
+}
+
+// Соединямся с БД
+$link=mysqli_connect("localhost", "root", "", "testdip");
+
+if(isset($_POST['submit']))
+{
+    // Вытаскиваем из БД запись, у которой логин равняеться введенному
+    $query = mysqli_query($link,"SELECT `user_id`, `user_password` FROM `user` WHERE `user_login`='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
+    $data = mysqli_fetch_assoc($query);
+
+    // Сравниваем пароли
+    if($data['user_password'] === md5(md5($_POST['password'])))
+    {
+        // Генерируем случайное число и шифруем его
+        $hash = md5(generateCode(10));
+
+        if(!empty($_POST['not_attach_ip']))
+        {
+            // Если пользователя выбрал привязку к IP
+            // Переводим IP в строку
+            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
+        }
+
+        // Записываем в БД новый хеш авторизации и IP
+        mysqli_query($link, "UPDATE `user` SET `user_hash`='".$hash."' ".$insip." WHERE `user_id`='".$data['user_id']."'");
+
+        // Ставим куки
+        setcookie("id", $data['user_id'], time()+60*60*24*30, "/");
+        setcookie("hash", $hash, time()+60*60*24*30, "/", null, null, true); // httponly !!!
+
+        // Переадресовываем браузер на страницу проверки нашего скрипта
+        header("Location: http://test-diplom/check.php"); exit();
+    }
+    else
+    {
+        print "Вы ввели неправильный логин/пароль";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col align-self-start">
+      </div>
+      <div class="col align-self-center">
+        <form method="post">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input name="login"type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="form-group form-check">
+            <input name = "not_attach_ip" type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Прикрепить ip (для безопасности)</label>
+          </div>
+          <button name="submit" type="submit" class="btn btn-success">Submit</button>
+        </form>
+      </div>
+      <div class="col align-self-end">
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
